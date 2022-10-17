f0299c5f0cd244c579c5f7f7d1e88742b0eea2b0
yuandou168 <74132047+yuandou168@users.noreply.github.com>
2022-10-17 16:18:30 +0200

    Update for.sh

--- nn-cifar-cl-yuandou168/for.sh
+++ nn-cifar-cl-yuandou168/for.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for i in 5, 10, 20, 30
+for i in 5 10 20 30
 do  
-    for j in 8, 16, 32, 64, 128
+    for j in 8 16 32 64 128
         do 
             # echo python $j, $i
             # python hello.py --id=0 --BATCH_SIZE=$j --epochs=$i
             python nn-cifar-cl-yuandou168.py --id=0 --BATCH_SIZE=$j --epochs=$i
+            sleep 6
         done
 done
