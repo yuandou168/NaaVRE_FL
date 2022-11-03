a20fe6215a9fa4ea7a9d39d43e436f3acc0f7910
yuandou168 <74132047+yuandou168@users.noreply.github.com>
2022-11-03 13:37:35 +0100

    fetch-laz-files-skoulouzis update

--- fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
+++ fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
@@ -1,6 +1,6 @@
-from laserfarm.remote_utils import get_wdclient
-import pathlib
 from laserfarm.remote_utils import list_remote
+import pathlib
+from laserfarm.remote_utils import get_wdclient
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -15,19 +15,18 @@ id = args.id
 
 
 
-conf_remote_path_ahn = conf_remote_path_root + '/ahn'
-conf_hostname = 'https://lifewatch.lab.uvalight.net:32443'
-conf_login = 'MTg5OWI3YjZkYTUyOT'
 conf_password = 'cxYTBmMjgxZWZi'
+conf_remote_path_ahn = conf_remote_path_root + '/ahn'
+conf_login = 'MTg5OWI3YjZkYTUyOT'
+conf_hostname = 'https://lifewatch.lab.uvalight.net:32443'
 
-conf_remote_path_ahn = conf_remote_path_root + '/ahn'
-conf_hostname = 'https://lifewatch.lab.uvalight.net:32443'
-conf_login = 'MTg5OWI3YjZkYTUyOT'
 conf_password = 'cxYTBmMjgxZWZi'
+conf_remote_path_ahn = conf_remote_path_root + '/ahn'
+conf_login = 'MTg5OWI3YjZkYTUyOT'
+conf_hostname = 'https://lifewatch.lab.uvalight.net:32443'
 conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
 laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
-print(laz_files)
 
 import json
 filename = "/tmp/laz_files_" + id + ".json"
