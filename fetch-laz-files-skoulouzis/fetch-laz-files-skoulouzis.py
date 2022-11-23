1fad77256b3e342757c811284c9f14d19ef9fe33
yuandou168 <74132047+yuandou168@users.noreply.github.com>
2022-11-23 12:35:05 +0100

    fetch-laz-files-skoulouzis update

--- fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
+++ fetch-laz-files-skoulouzis/fetch-laz-files-skoulouzis.py
@@ -1,6 +1,6 @@
 from laserfarm.remote_utils import list_remote
-from laserfarm.remote_utils import get_wdclient
 import pathlib
+from laserfarm.remote_utils import get_wdclient
 
 import argparse
 arg_parser = argparse.ArgumentParser()
@@ -8,23 +8,24 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('--id', action='store', type=str, required=True, dest='id')
 
 
+arg_parser.add_argument('--param_hostname', action='store', type=str, required='True', dest='param_hostname')
+arg_parser.add_argument('--param_login', action='store', type=str, required='True', dest='param_login')
+arg_parser.add_argument('--param_password', action='store', type=str, required='True', dest='param_password')
 
 args = arg_parser.parse_args()
+print(args)
 
 id = args.id
 
 
+param_hostname = args.param_hostname
+param_login = args.param_login
+param_password = args.param_password
 
-conf_remote_path_ahn = conf_remote_path_root + '/ahn'
-conf_password = ''
-conf_hostname = ''
-conf_login = ''
+conf_remote_path_ahn =  '/webdav/pointcloud' + '/ahn'
 
-conf_remote_path_ahn = conf_remote_path_root + '/ahn'
-conf_password = ''
-conf_hostname = ''
-conf_login = ''
-conf_wd_opts = { 'webdav_hostname': conf_hostname, 'webdav_login': conf_login, 'webdav_password': conf_password}
+conf_remote_path_ahn =  '/webdav/pointcloud' + '/ahn'
+conf_wd_opts = { 'webdav_hostname': param_hostname, 'webdav_login': param_login, 'webdav_password': param_password}
 laz_files = [f for f in list_remote(get_wdclient(conf_wd_opts), pathlib.Path(conf_remote_path_ahn).as_posix())
              if f.lower().endswith('.laz')]
 
